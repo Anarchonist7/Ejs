364fea9e9f86e373ab8518c739bc1ac27e6aa9d4
Dawson Ley <anarchonist@protonmail.com>
2018-10-29 05:33:39 +0000

    fixed some glitches, footer still not appearing on about page but at least styles are applying for the most part

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 'use strict';
 
-//get all the tools we need
+// get all the tools we need
 
 var express = require('express');
-var routes = require('./routes.index.js');
-var port = proess.env.PORT || 3000;
+var routes = require('./routes/index.js');
+var port = process.env.PORT || 3000;
 
 var app = express();
 
-//setup our express application
+
+
+// setup our express application
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
 
 
-// setup routes
 
+// setup routes
 routes(app);
 
-//start our server
-
+// start our server
 app.listen(port, function() {
-  console.log('Server listening on port ' + port + '...');
+    console.log('Server listening on port ' + port + '...');
 });
